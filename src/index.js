05c40a3edeb9a2d3e136a60c5aa7c718f85a9439
Maksym Kantor <max.kantor72@gmail.com>
2022-01-19 14:47:00 +0200

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let arr = [];
+    if (n > 0) {
+       arr = String(n).split('');
+    }else {
+        arr = String(n).split('');
+        arr.shift();
+    }
+    return Number(arr.reverse().join(''));
 }
